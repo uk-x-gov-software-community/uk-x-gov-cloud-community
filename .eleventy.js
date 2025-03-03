3ea73111e6752737bf269981618c0e769f206fae
Benjamin Bennett <benjamin.bennett@digital.cabinet-office.gov.uk>
2025-03-03 08:53:52 +0000

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,50 @@
+const govukEleventyPlugin = require('@x-govuk/govuk-eleventy-plugin')
+
+module.exports = function(eleventyConfig) {
+  
+  eleventyConfig.setLiquidOptions({
+    dynamicPartials: false,
+    strictFilters: false,
+  });
+  return {
+   addPassthroughCopy: function (dir) {
+     eleventyConfig.addPassthroughCopy(dir)
+   },
+  };
+}
+
+
+ module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('assets')
+  eleventyConfig.addPassthroughCopy('open-source-presentation/*.png')
+ 
+   // Register the plugin
+   eleventyConfig.addPlugin(govukEleventyPlugin,{
+    fontFamily: 'arial, sans-serif',
+    header: {
+      logotype: {
+        html: '<img id="xgov-logo" src="/../../../assets/logo/cloud_pink.svg" alt="Cross Government Cloud Community">',
+      },
+       productName: 'Cross Government Cloud Community',
+    },
+    
+    footer: {
+      contentLicence: {
+        html: 'A community project. <a class="govuk-footer__link" href="https://github.com/uk-x-gov-software-community/uk-x-gov-cloud-community.github.io">GitHub source</a>.<br /> Thanks to <a href="https://x-govuk.github.io/govuk-eleventy-plugin/" class="govuk-link" >X-GOVUK projects</a> for the template' 
+      },
+      copyright: {
+        html: 'Responsibility &#9745; Pragmatism  &#9745; Public Service  &#9745; <br /> © Cross Government Cloud Community'
+      }
+    }
+   })
+   
+   return {
+     dataTemplateEngine: 'njk',
+     htmlTemplateEngine: 'njk',
+     markdownTemplateEngine: 'njk',
+     dir: {
+       // Use layouts from the plugin
+       layouts: 'node_modules/@x-govuk/govuk-eleventy-plugin/layouts'
+     }
+   }
+ };
---
+++ index.md
@@ -0,0 +1,32 @@
+---
+homepage: true
+layout: product
+order: 5
+title: Welcome
+description: We are a multidisciplinary group with a collective interest in cross-government cloud.
+image:
+  src: "../../assets/logo/cloud_pink.svg"
+  alt: "Cross Governemnt Cloud"   
+
+---
+
+
+## Our Vision
+To provide a forum for multidisciplinary cross-government collaboration on cloud:
+*	To help each other in the adoption and maturation of our use of cloud
+*	To share case studies, and lessons learned
+*	To discover and evaluate emerging trends in cloud usage
+
+## How are we doing this?
+* Engaging with the community on [UK Government Digital](https://ukgovernmentdigital.slack.com/) Slack - in the [#cloud](https://ukgovernmentdigital.slack.com/archives/C04FA6GL49J) channel
+*	We have a small facilitation group that is doing some of the initial setup
+
+## Do you want to help facilitate the group?
+Drop an email to [Benjamin Bennett](mailto:benjamin.bennett@digital.cabinet-office.gov.uk) or [Chris Hesketh](mailto:chris.hesketh@digital.cabinet-office.gov.uk) and we'll reach out.
+
+## Join us!
+* Chat with us in [`#cloud`](https://ukgovernmentdigital.slack.com/archives/C04FA6GL49J)
+  * [How to use cross-government Slack](https://x-govuk.github.io/posts/how-to-use-cross-government-slack/). 
+
+## Contributing to this site
+Pull requests are welcomed for this site see the readme on our [GitHub Repo](https://github.com/uk-x-gov-software-community/uk-x-gov-cloud-community/) for more details.
